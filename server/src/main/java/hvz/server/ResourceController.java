d133d2b6dfa4ce4a46c9095d8adc262c881e3f46
Paul Steele <paul_steele@live.com>
2015-02-25 20:03:46 -0500

    set up /user/register

--- server/src/main/java/hvz/server/ResourceController.java
+++ server/src/main/java/hvz/server/ResourceController.java
@@ -1,6 +1,7 @@
 package hvz.server;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ResourceController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/user")
-    public UserResource UserResource(@RequestParam(value="name", defaultValue="World") String name) {
-        return new UserResource(counter.incrementAndGet(), String.format(template, name));
+    @RequestMapping("/user/register")
+    public String registerPlayer (@RequestParam(value="username", required = false) String username, 
+    								@RequestParam(value = "feedcode" , required = false) String feedcode , 
+    								@RequestParam(value = "password", required = false) String password) {
+		//Set up response object
+		JSONObject response = new JSONObject();
+    	try {
+    		//verify that all parameters are valid
+    		if (username == null || feedcode == null || password == null){
+    			response.put("success", false);
+    		}
+    		else {
+    			response.put("success", true);
+    			response.put("username", username);
+    			response.put("feedcode", feedcode);
+    			response.put("password", password);
+    		}
+    		
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return response.toString();
     }
 }
