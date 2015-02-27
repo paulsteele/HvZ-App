8ffd564d6d5c61ff94778544689c1f661df7df57
kgrodd <kgrodd@purdue.edu>
2015-02-26 22:57:12 -0500

    some methods added

--- DB/java_Database_Interface.java
+++ DB/java_Database_Interface.java
@@ -1,10 +1,23 @@
 import java.sql.*;
 
 public class java_Database_Interface{
-	public static void main(String [] args){
+	public static  Connection connect(){
 		try{
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:database.db");
+			return conn;
 		}
-		catch(Exception e){}
+		catch(Exception e){
+			System.out.println("Connection failed");
+			return null;
+		}
+	}
+	public static void addPlayer(int id, Connection c){}
+	public static void removePlayer(int id, Connection c){}
+	public static void makeZombie(int id, Connection c){}
+	public static void makeHuman(int id, Connection c){}
+	//Statement s = c.createStatement();
+	
+	public static void main (String []args){//for testing
+		Connection c = connect();
 	}
 }
