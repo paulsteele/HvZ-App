dab5b556a02e636a0fee6b1560b68875fefd01d7
Paul Steele <paul_steele@live.com>
2015-02-27 18:00:13 -0500

    Server mapping explained, seperate feecode generation added

--- server/src/main/java/hvz/server/ResourceController.java
+++ server/src/main/java/hvz/server/ResourceController.java
@@ -1,7 +1,9 @@
 package hvz.server;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +29,8 @@ public class ResourceController {
     			String feedcodeVal;
     			if (feedcode == null){
     				//need to generate a feedcode
-    				feedcodeVal = Server.generateFeedcode(admin);
+    				JSONObject generated = new JSONObject(new JSONTokener(generateFeedcode(admin)));
+    				feedcodeVal = generated.getString("feedcode");
     			}
     			else {
     				//player supplied
@@ -131,6 +134,35 @@ public class ResourceController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	return response.toString();
+    }
+    
+    @RequestMapping ("feedcode/generate")
+    public String generateFeedcode(@RequestParam(value = "admin", defaultValue = "false")boolean admin){
+    	//generate a unique string code
+    	String feedcode = RandomStringUtils.randomAlphanumeric(ServerConfiguration.feedcodeLength -1);
+    	//add prefix based on player or admin
+    	char prefix;
+    	if (admin)
+    		prefix = ServerConfiguration.adminPrefix;
+    	else
+    		 prefix = ServerConfiguration.playerPrefix;
+    	
+    	feedcode = prefix + feedcode.toUpperCase();
+    	//verify that feedcode isn't taken
+    	
+    	
+    	
+    	//Set up response object
+    	JSONObject response = new JSONObject();
+    	try {
+			response.put("success", true);
+			response.put("feedcode", feedcode);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     	return response.toString();
     }
     
--- server/src/main/java/hvz/server/Server.java
+++ server/src/main/java/hvz/server/Server.java
@@ -1,6 +1,5 @@
 package hvz.server;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -18,19 +17,4 @@ public class Server {
     	SpringApplication.run(Server.class, args);
     }
     
-    public static String generateFeedcode(boolean admin){
-    	//generate a unique string code
-    	String feedcode = RandomStringUtils.randomAlphanumeric(ServerConfiguration.feedcodeLength -1);
-    	//add prefix based on player or admin
-    	char prefix;
-    	if (admin)
-    		prefix = ServerConfiguration.adminPrefix;
-    	else
-    		 prefix = ServerConfiguration.playerPrefix;
-    	
-    	feedcode = prefix + feedcode.toUpperCase();
-    	//verify that feedcode isn't taken
-    	
-    	return feedcode;
-    }
 }
