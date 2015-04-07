9c74ad4babec67021c66bea019769945819ac0e7
goelm <goelm@purdue.edu>
2015-04-07 18:42:48 -0400

    mission screens added

---
+++ HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/CreateMissionActivity.java
@@ -0,0 +1,37 @@
+package edu.purdue.cs.hvzmasterapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+/**
+ * Created by manasigoel on 4/7/15.
+ */
+public class CreateMissionActivity {
+    Server server = Server.getInstance();
+
+    PopupWindow popup;
+
+    public void createMission(View view){
+        //String name = ((EditText)findViewById(R.id.missionName)).getText().toString();
+        //String objectives = ((EditText)findViewById(R.id.missionObjectives)).getText().toString();
+        /*int status = server.addMission(name, ojbectives);
+        if (status == 0) {
+            msg.setText("Success!");
+            msg.setTextColor(Color.GREEN);
+            msg.setVisibility(View.VISIBLE);
+            Intent intent = new Intent();
+        }
+        else {
+            msg.setText("Mission could not be added");
+            msg.setTextColor(Color.RED);
+            msg.setVisibility(View.VISIBLE);
+        }*/
+    }
+}
+
