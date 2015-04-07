1a89ce7a9660a2cb4b789548158f4e30691edf47
Wells Lin <lin331@purdue.edu>
2015-04-07 18:15:39 -0400

    Added client ability to add user to game

---
+++ HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/CreateGameActivity.java
@@ -0,0 +1,28 @@
+package edu.purdue.cs.hvzmasterapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by Wells on 4/7/2015.
+ */
+public class CreateGameActivity extends ActionBarActivity {
+    Server server = Server.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void submit(View view) {
+        String name = ((EditText)findViewById(R.id.nameinput)).getText().toString();
+        int status = server.createGame();
+    }
+}
