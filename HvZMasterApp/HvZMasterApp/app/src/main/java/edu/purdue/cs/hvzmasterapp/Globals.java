3f9957a91fc359801e088f37f7f6c023f9e08b42
Wells Lin <lin331@purdue.edu>
2015-04-09 16:39:23 -0400

    Added more Globals methods

--- HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/Globals.java
+++ HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/Globals.java
@@ -22,6 +22,18 @@ public class Globals {
         this.self = self;
     }
 
+    public Boolean isAdmin() {
+        return self.isAdmin;
+    }
+
+    public Boolean isZombie() {
+        return self.isZombie;
+    }
+
+    public String getFeedCode() {
+        return self.uniqueID;
+    }
+
     public String getGameCode() {
         return self.gameID;
     }
