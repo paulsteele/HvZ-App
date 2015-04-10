2d55dacdd1febe60f4f5863690edb5de70ce0d8a
goelm <goelm@purdue.edu>
2015-04-09 20:47:57 -0400

    mission list

--- HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/MissionListActivity.java
+++ HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/MissionListActivity.java
@@ -1,5 +1,6 @@
 package edu.purdue.cs.hvzmasterapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -27,7 +28,7 @@ import java.util.ArrayList;
  */
 public class MissionListActivity extends ActionBarActivity{
 
-    ArrayList<Game> missionList;
+    ArrayList<Mission> missionList;
     ArrayList<String> itemList = new ArrayList<>();
     Server server = Server.getInstance();
     private Globals global = Globals.getInstance();
@@ -38,14 +39,57 @@ public class MissionListActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_list);
+        setContentView(R.layout.activity_mission_list);
+    }
 
-        /*missionList = server.getMissionList();
+    private class MissionAdapter extends ArrayAdapter<Mission> {
+
+        public MissionAdapter(Context context, ArrayList<Mission> missions) {
+            super(context, 0, missions);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            Mission mission = getItem(position);
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.missionItem, parent, false);
+            }
+            // Lookup view for data population
+            TextView title = (TextView) convertView.findViewById(R.id.missionTitle);
+            TextView humanObjectives = (TextView) convertView.findViewById(R.id.missionHuman);
+            TextView zombieObjectives = (TextView) convertView.findViewById(R.id.missionZombie);
+            // Populate the data into the template view using the data object
+            title.setText(mission.getTitle() + "\n");
+            humanObjectives.setText("Human Objectives: " + mission.getHumanObjective());
+            zombieObjectives.setText("Zombie Objectives: " + mission.getZombieObjective());
+            // Return the completed view to render on screen
+            return convertView;
+        }
+    }
+
+    public void refresh(){
+        missionList = server.getMissionList(global.getGameCode());
         if (missionList == null) {
             missionList = new ArrayList<>();
-            missionList.add(new Game("There are no missions", null, null));
+            missionList.add(new Mission("There are no missions", null, null));
         }
-        list = (ListView) findViewById(R.id.missionListView);*/
+        list = (ListView) findViewById(R.id.missionListView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                refresh();
+                finish();
+            }
+        }
+    }
+
+    public void listMissions(View view){
+
     }
 
 }
