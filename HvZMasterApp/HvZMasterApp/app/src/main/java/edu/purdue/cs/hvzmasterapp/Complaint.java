c186419acb535b11d80af721b21f417e70e6ff1b
Wells Lin <lin331@purdue.edu>
2015-04-29 19:21:28 -0400

    Added function to delete complaints

--- HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/Complaint.java
+++ HvZMasterApp/HvZMasterApp/app/src/main/java/edu/purdue/cs/hvzmasterapp/Complaint.java
@@ -4,13 +4,25 @@ package edu.purdue.cs.hvzmasterapp;
  * Created by Wells on 4/28/2015.
  */
 public class Complaint {
-    String id;
-    String sender;
-    String message;
+    private String id;
+    private String sender;
+    private String message;
 
     public Complaint(String id, String message, String sender) {
         this.id = id;
         this.message = message;
         this.sender = sender;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getID() {
+        return id;
     }
 }
